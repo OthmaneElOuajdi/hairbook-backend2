73f5f94fd45943a1e5800a2bcc117e583fbdf61a
Othmane <oelouajdi@hotmail.com>
2025-07-23 21:36:06 +0200

    Premier commit : création du projet backend

---
+++ hairbook-backend/src/main/java/com/hairbook/hairbook_backend/HairbookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hairbook.hairbook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairbookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairbookBackendApplication.class, args);
+	}
+
+}
